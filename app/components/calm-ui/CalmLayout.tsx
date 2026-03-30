449554d6d93de2a45893226f25abf10fe4ee5726
Jatin Singh <jatin.singh@verihireai.work>
2026-03-30 16:58:14 +0530

    Calm Room Changes v2

--- app/components/calm-ui/CalmLayout.tsx
+++ app/components/calm-ui/CalmLayout.tsx
@@ -2,35 +2,23 @@
 
 import InterviewHeader from "./InterviewHeader";
 import CameraPanel from "./CameraPanel";
-import OrbQuestion from "@/app/components/calm/OrbQuestion";
 import TelemetryIndicators from "./TelemetryIndicators";
 
 export default function CalmLayout() {
   return (
-    <div className="h-[100dvh] w-full bg-black text-white flex flex-col overflow-hidden">
-
-      {/* HEADER */}
+    <div className="flex h-[100dvh] w-full flex-col overflow-hidden bg-black text-white">
       <div className="h-[80px] flex-shrink-0 border-b border-white/10">
         <InterviewHeader />
       </div>
 
-      {/* MAIN CONTENT */}
-        <div className="flex-1 flex flex-col items-center justify-start pt-6 gap-8">
-        {/* CAMERA */}
+      <div className="flex flex-1 flex-col items-center justify-start gap-8 pt-6">
         <div className="flex flex-col items-center">
           <CameraPanel />
-<       div className="w-[300px] h-[1px] bg-white/5 mt-4 mb-2" />
-          {/* ✅ TELEMETRY BELOW CAMERA */}
+          <div className="mt-4 h-px w-[300px] bg-white/5" />
           <div className="mt-4">
             <TelemetryIndicators />
           </div>
         </div>
-
-        {/* ORB CENTER */}
-        <div className="flex items-center justify-center w-full flex-1">
-        <OrbQuestion />
-        </div>
-
       </div>
     </div>
   );
--- app/components/calm-ui/QuestionPanel.tsx
+++ app/components/calm-ui/QuestionPanel.tsx
@@ -1,9 +1,3 @@
-import OrbQuestion from "../calm/OrbQuestion";
-
 export default function QuestionPanel() {
-  return (
-    <div className="flex items-center justify-center">
-      <OrbQuestion />
-    </div>
-  );
+  return null;
 }
